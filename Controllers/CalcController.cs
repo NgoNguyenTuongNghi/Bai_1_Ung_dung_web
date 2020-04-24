a2bcaf9f61b34aa66fd4dc8431846068789daa0a
Tường Nghi <62927599+NgoNguyenTuongNghi@users.noreply.github.com>
2020-04-24 16:30:12 +0700

    Initial commit

---
+++ Controllers/CalcController.cs
@@ -0,0 +1,44 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new calc());
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(calc c, string calculate)
+        {
+            switch (calculate)
+            {
+                case "add":
+                    c.total = "" + (c.val1+c.val2);
+                    break;
+                case "min":
+                    c.total = "" + (c.val1-c.val2);
+                    break;
+                case "sub":
+                    c.total = "" + (c.val1*c.val2);
+                    break;
+                case "dii":
+                    if (c.val2 == 0) 
+                        c.total = "Không chia được cho 0"; 
+                    else 
+                        c.total = "" + (c.val1/c.val2); 
+                    break;
+                    
+            }
+            return View(c);
+        }
+    }
+}
---
+++ Models/calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+    public class calc
+    {
+        public float val1 { get; set; }
+
+        public float val2 { get; set; }
+
+        public string total { get; set; }
+    }
+}
